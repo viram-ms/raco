16f9c08bf1ebd436593287451f953249c4072981
viram shah <viram1999@gmail.com>
2024-08-12 12:19:47 +0530

    initial commit

---
+++ hin_permissions/clinical/provider.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_version = "~>1.3.0"
+
+  cloud {
+    # organization = "hca-healthcare"
+
+    # workspaces {
+    #   tags = ["env:#{environment}#", "data-domain:clinical"]
+    # }
+  }
+}
